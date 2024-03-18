9dfdf4d1ee082ac4939992da572d0403fa71fa1f
hien <hienvd6@fsoft.com.vn>
2024-03-18 20:10:27 +0700

    views va urls cho list_cve

--- CVEAlert/firstapp/urls.py
+++ CVEAlert/firstapp/urls.py
@@ -15,10 +15,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
 from firstapp import views as firstapp 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', firstapp.get_home),
-    path('firstapp/', include('firstapp.urls'))
+    path('', firstapp.get_list_CVE,name='list_cves')
 ]
--- CVEAlert/firstapp/views.py
+++ CVEAlert/firstapp/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
-
+from .models import CVE 
 # Create your views here.
 def get_home(request):
     return render(request,'home.html')
-    
+
+def get_list_CVE(request):
+    listCVE= CVE.objects.all
+    return render(request, 'firstapp/list_cves.html')   
