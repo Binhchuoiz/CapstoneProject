ad027267849285428bce72aaa8b7457b8624bcee
Nguyen Duc Binh <binhndhe161032@fpt.edu.vn>
2024-03-19 10:17:37 +0700

    front-end: fix logic home + navbar + URL

--- CVEAlert/CVEAlert/settings.py
+++ CVEAlert/CVEAlert/settings.py
@@ -75,15 +75,22 @@ WSGI_APPLICATION = 'CVEAlert.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'security_news',
+#         'USER': 'root',
+#         'PASSWORD': '',
+#         'HOST': 'localhost',
+#         'PORT': '3306',
+#     }
+# }
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'security_news',
-        'USER': 'root',
-        'PASSWORD': '',
-        'HOST': 'localhost',
-        'PORT': '3306',
-    }
+'default': {
+'ENGINE': 'django.db.backends.sqlite3',
+'NAME': BASE_DIR / 'db.sqlite3',
+}
 }
 
 
--- CVEAlert/firstapp/urls.py
+++ CVEAlert/firstapp/urls.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 from django.urls import path
-from firstapp import views as firstapp 
+from .views import get_home, get_list_CVE
 
 app_name = 'app'
 
 urlpatterns = [
-    path('', firstapp.get_list_CVE,name='list_cves')
+    path('', get_home,name='home'),
+    path('list-cve/', get_list_CVE,name='list_cves')
 ]
