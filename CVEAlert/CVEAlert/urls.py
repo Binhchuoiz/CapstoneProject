f964cf0839a5e3dbf8625aedffdd5268d1a3d590
Nguyen Duc Binh <binhndhe161032@fpt.edu.vn>
2024-03-19 16:29:58 +0700

    front-end: fĩ picture

--- CVEAlert/CVEAlert/urls.py
+++ CVEAlert/CVEAlert/urls.py
@@ -1,7 +1,13 @@
 from django.contrib import admin
 from django.urls import path, include
-from firstapp import views as firstapp 
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.conf.urls.static import static
+from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('firstapp.urls'))
 ]
+
+
+urlpatterns += staticfiles_urlpatterns()
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
