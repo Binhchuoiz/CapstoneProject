8c3479696bf9b219c2e4e610340e5f18aafed810
BADly1122 <ducba3011@gmail.com>
2024-03-20 03:03:09 +0700

    fix models.py, update views.py and list_cves.html to add print statement to listCVE iteration in HTML template

--- CVEAlert/firstapp/models.py
+++ CVEAlert/firstapp/models.py
@@ -11,71 +11,71 @@ class CVE(models.Model):
     assigner_Org_Id = models.CharField(max_length=255, default="", null = True)
     provider_Metadata = models.CharField(max_length=255, default="", null = True)
 
-    def __str__(self) -> str:
-        return super(self.cve_id).__str__(self.cve_id)
+    def __str__(self):
+        return self.cve_id
     
 class Descriptions(models.Model):
     con = models.ForeignKey(CVE, on_delete=models.CASCADE, related_name='description_cves', blank=True, default=None)
     value = models.CharField(max_length=9000, default="", null = True)
 
-    def __str__(self) -> str:
-        return super(self.value).__str__(self.value)
+    def __str__(self):
+        return self.value
     
 class Solutions(models.Model):
     con = models.ForeignKey(CVE, on_delete=models.CASCADE, related_name='solutions_cves', blank=True, default=None)
     value = models.CharField(max_length=9000, default="", null = True)
 
-    def __str__(self) -> str:
-        return super(self.value).__str__(self.value)
+    def __str__(self):
+        return self.value
     
 class Versions(models.Model):
     version = models.CharField(max_length=3000, default="", null = True)
     status = models.CharField(max_length=255, default="", null = True)
 
-    def __str__(self) -> str:
-        return super(self.version).__str__(self.version)
+    def __str__(self):
+        return self.version
     
 class Products(models.Model):
     name = models.CharField(max_length=3000, default="", null = True)
 
-    def __str__(self) -> str:
-        return super(self.name).__str__(self.name)
+    def __str__(self):
+        return self.name
     
 class Vendors(models.Model):
     name = models.CharField(max_length=3000, default="", null = True)
 
-    def __str__(self) -> str:
-        return super(self.name).__str__(self.name)
+    def __str__(self):
+        return self.name
     
 class Products_Versions(models.Model):
     product = models.ForeignKey(Products, on_delete=models.CASCADE, related_name='product_version', blank=True, default=None)
     version = models.ForeignKey(Versions, on_delete=models.CASCADE, related_name='version_product', blank=True, default=None)
     
-    def __str__(self) -> str:
-        return super(self.product).__str__(self.product)
+    def __str__(self):
+        return self.product
     
 class Affected(models.Model):
     con = models.ForeignKey(CVE, on_delete=models.CASCADE, related_name='affected_cves', blank=True, default=None)
     product = models.ForeignKey(Products, on_delete=models.CASCADE, related_name='product_vendor', blank=True, default=None)
     vendor = models.ForeignKey(Vendors, on_delete=models.CASCADE, related_name='vendor_product', blank=True, default=None)
 
-    def __str__(self) -> str:
-        return super(self.product).__str__(self.product)
+    def __str__(self):
+        return self.product
     
 class References(models.Model):
     con = models.ForeignKey(CVE, on_delete=models.CASCADE, related_name='references_cves', blank=True, default=None)
     url = models.CharField(max_length=3000, default="", null = True)
 
-    def __str__(self) -> str:
-        return super(self.url).__str__(self.url)
+    def __str__(self):
+        return self.url
     
 class CvssV20(models.Model):
     version = models.CharField(max_length=255, default="", null = True)
     vector_string = models.CharField(max_length=300, default="", null = True)
     base_score = models.FloatField(default="", null = True)
 
-    def __str__(self) -> str:
-        return super(self.version).__str__(self.version)
+    def __str__(self):
+        return self.version
     
 class CvssV30(models.Model):
     version = models.CharField(max_length=255, default="", null = True)
@@ -83,8 +83,8 @@ class CvssV30(models.Model):
     base_score = models.FloatField(default="", null = True)
     base_severity = models.CharField(max_length=255, default="", null = True)
 
-    def __str__(self) -> str:
-        return super(self.version).__str__(self.version)
+    def __str__(self):
+        return self.version
     
 class CvssV31(models.Model):
     version = models.CharField(max_length=255, default="", null = True)
@@ -92,8 +92,8 @@ class CvssV31(models.Model):
     base_score = models.FloatField(default="", null = True)
     base_severity = models.CharField(max_length=255, default="", null = True)
 
-    def __str__(self) -> str:
-        return super(self.version).__str__(self.version)
+    def __str__(self):
+        return self.version
     
 class Metric(models.Model):
     con = models.ForeignKey(CVE, on_delete=models.CASCADE, related_name='metric_cve', blank=True, default=None)
@@ -101,6 +101,6 @@ class Metric(models.Model):
     cvssv30 = models.ForeignKey(CvssV30, on_delete=models.CASCADE, related_name='metric_cvss_v30', blank=True, default=None)
     cvssv31 = models.ForeignKey(CvssV31, on_delete=models.CASCADE, related_name='metric_cvss_v31', blank=True, default=None)
     
-    def __str__(self) -> str:
-        return super(self.con).__str__(self.con)
+    def __str__(self):
+        return self.con
 # Create your models here.
--- CVEAlert/firstapp/views.py
+++ CVEAlert/firstapp/views.py
@@ -7,19 +7,20 @@ def get_home(request):
     return render(request,'home.html')
 
 def get_list_CVE(request):
-    listCVE= CVE.objects.all
+    listCVE= CVE.objects.all()
     if request.method == 'POST':  
         # if 'search_focus' in request.POST:
-        #    id_cve= request.POST['search_focus']
-        #    listCVE = CVE.objects.filter(title_contains=id_cve)
+        # id_cve= request.POST['search_focus']
+        # listCVE = CVE.objects.filter(title_contains=id_cve)
         if 'newest' in request.POST:
             listCVE= CVE.objects.all().order_by('-date_publish')
         elif  'oldest' in request.POST:
-         listCVE= CVE.objects.all().order_by('date_publish')
+            listCVE= CVE.objects.all().order_by('date_publish')
+    print(listCVE)     
 
      
     context={
-        #'list_cve':[1,2,3,4],
-        'list_cve':listCVE
-         }
+        # 'list_cve':[1,2,3,4],
+        'listCVE':listCVE
+    }
     return render(request, 'firstapp/list_cves.html', context=context)   
