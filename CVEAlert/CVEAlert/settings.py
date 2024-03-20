2f81e4c0f3b86637692b896a85383845f55134b9
Nguyen Duc Binh <binhndhe161032@fpt.edu.vn>
2024-03-20 23:47:11 +0700

    Front-End: Fix logic for detail_cve href

--- CVEAlert/CVEAlert/settings.py
+++ CVEAlert/CVEAlert/settings.py
@@ -71,7 +71,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'test',
         'USER': 'root',
-        'PASSWORD': 'Bad11224791025@',
+        'PASSWORD': 'Hanoi123789@',
         'HOST': 'localhost',
         'PORT': '3306',
     }
