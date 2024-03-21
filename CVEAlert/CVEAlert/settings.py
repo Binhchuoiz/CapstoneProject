220150753ebaa0f9e36a55112078e122b63bdf55
Nguyen Duc Binh <binhndhe161032@fpt.edu.vn>
2024-03-21 21:47:46 +0700

    front-end: add logic show year in list.html

--- CVEAlert/CVEAlert/settings.py
+++ CVEAlert/CVEAlert/settings.py
@@ -70,23 +70,23 @@ WSGI_APPLICATION = 'CVEAlert.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'test',
-        'USER': 'root',
-        'PASSWORD': '',
-        'HOST': 'localhost',
-        'PORT': '3306',
-    }
-}
-
 # DATABASES = {
-# 'default': {
-# 'ENGINE': 'django.db.backends.sqlite3',
-# 'NAME': BASE_DIR / 'db.sqlite3',
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'test',
+#         'USER': 'root',
+#         'PASSWORD': '',
+#         'HOST': 'localhost',
+#         'PORT': '3306',
+#     }
 # }
-# }
+
+DATABASES = {
+'default': {
+'ENGINE': 'django.db.backends.sqlite3',
+'NAME': BASE_DIR / 'db.sqlite3',
+}
+}
 
 
 # Password validation
