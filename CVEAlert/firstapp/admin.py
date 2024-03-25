1d99a50c4315e2979d888934cf80bc313f8072fe
Nguyen Duc Binh <binhndhe161032@fpt.edu.vn>
2024-03-25 11:25:42 +0700

    BE: BE: Đăng ký adminsite cho 1 số model

--- CVEAlert/firstapp/admin.py
+++ CVEAlert/firstapp/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
-
+from . import models
 # Register your models here.
+
+admin.site.register(models.CVE)
+admin.site.register(models.Metric)
+admin.site.register(models.CvssV31)
+admin.site.register(models.CvssV30)
+admin.site.register(models.CvssV20)
+admin.site.register(models.References)
+admin.site.register(models.Affected)
+admin.site.register(models.Vendors)
+admin.site.register(models.Versions)
+admin.site.register(models.Products)
+admin.site.register(models.Products_Versions)
