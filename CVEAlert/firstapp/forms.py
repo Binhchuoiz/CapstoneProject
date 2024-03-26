ffa0dd44a07932edbca58ae433296dde29dcf735
hien <zdemon2002@gmail.com>
2024-03-26 17:44:08 +0700

    add forms

--- CVEAlert/firstapp/forms.py
+++ CVEAlert/firstapp/forms.py
@@ -1,1 +1,16 @@
 from django import forms
+from ckeditor.fields import RichTextField
+from .models import CVE,Affected
+
+class CVEform(forms.ModelForm):
+    class Meta:
+        model = CVE
+        fields = '__all__'
+
+        widgets = {
+            'description' : RichTextField(config_name='default')
+        }
+class AffectedForm(forms.ModelForm):
+    class Meta:
+        model = Affected
+        fields = '__all__'
