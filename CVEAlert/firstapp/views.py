b527de1ee749b748efd816e74ebb5052fb9e6c68
BADly1122 <ducba3011@gmail.com>
2024-03-28 01:04:56 +0700

    fix year filter

--- CVEAlert/firstapp/views.py
+++ CVEAlert/firstapp/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, HttpResponseRedirect
+from django.shortcuts import render, HttpResponseRedirect, redirect
 from django.urls import reverse
 from django.core.paginator import Paginator, EmptyPage , PageNotAnInteger
 from .models import CVE , Affected , References , Metric , CvssV31 , Products , Vendors , Descriptions , Solutions , Products_Versions , Follow_Affected 
@@ -25,19 +25,26 @@ def get_home(request):
     return render(request, 'home.html', context=context)
 
 def get_list_CVE(request, page):
-    listCVE= CVE.objects.all().order_by('date_publish')
-    year = CVE.objects.values_list('year',flat=True)
+    listCVE = CVE.objects.all().order_by('date_publish')
+    year = CVE.objects.values_list('year', flat=True)
     unique_Year = set(year)
     unique_year_List = list(unique_Year)
     unique_year_List.sort()
+    
+    selected_year = None
     if request.method == 'POST':
         selected_years = request.POST.getlist('filter_year')
         if selected_years:
+            selected_year = selected_years[0]  
             listCVE = CVE.objects.filter(year__in=selected_years)
-    if request.method == 'POST':  
         if 'search_focus' in request.POST:
-            id_cve= request.POST['search_focus']
-            listCVE = CVE.objects.filter(cve_id__contains=id_cve)
+            id_cve = request.POST['search_focus']
+            listCVE = listCVE.filter(cve_id__contains=id_cve)
+    else:
+        selected_year = request.GET.get('filter_year', None)
+        if selected_year:
+            listCVE = listCVE.filter(year=selected_year)
+    
     per_page = request.GET.get("per_page", 10)
     paginator = Paginator(listCVE, per_page)
     page_obj = paginator.get_page(page)
@@ -63,21 +70,20 @@ def get_list_CVE(request, page):
              affected = Affected.objects.filter(vendor__in=vendors_Name)   
     page_obj.affected = affected
     # print(unique_year_List)
-    context={
+    context = {
         "page": {
-			'prev': page_obj.number - 1 if page_obj.number - 1 > 0 else 1,
-			'current': page_obj.number,
-			'next': page_obj.number + 1 if page_obj.number + 1 < paginator.num_pages else paginator.num_pages,
-		},
+            'prev': page_obj.number - 1 if page_obj.number - 1 > 0 else 1,
+            'current': page_obj.number,
+            'next': page_obj.number + 1 if page_obj.number + 1 < paginator.num_pages else paginator.num_pages,
+        },
         'len_page': paginator.num_pages,
         'paginator': paginator,
-        'page_obj' : page_obj,
-        'products' : products,
-        'vendors' : vendors,
-        'affected' : affected,
-        'unique_year' : unique_year_List
-        
-
+        'page_obj': page_obj,
+        'products': products,
+        'vendors': vendors,
+        'affected': affected,
+        'unique_year': unique_year_List,
+        'selected_year': selected_year,
     }
 
     # print(listCVE)
