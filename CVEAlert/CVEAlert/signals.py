3d3ac225d5ff1f1d7e8f23b253297b337b0b2bc2
hien <zdemon2002@gmail.com>
2024-04-01 12:11:36 +0700

    test noti

--- CVEAlert/CVEAlert/signals.py
+++ CVEAlert/CVEAlert/signals.py
@@ -2,7 +2,7 @@ from django.db.models.signals import post_save
 from django.dispatch import receiver,Signal
 
 from .alert_tele import send_message_telegram , reformat_tele_message
-from firstapp.models import CVE , CvssV31 , Descriptions , User, Metric
+from firstapp.models import CVE , CvssV31 , Descriptions , User, Metric ,Follow_Affected
 from accounts.models import NotiUser
 
 cve_Updated = Signal()
@@ -10,7 +10,6 @@ cve_Updated = Signal()
 # @receiver(cve_Updated)
 # def new_cve_noti(sender, **kwargs):
 @receiver(post_save, sender=CVE)
-@receiver(post_save,sender=Metric)
 def new_cve_noti(sender, instance, created, **kwargs):   
         cve=instance
         try:
@@ -22,6 +21,10 @@ def new_cve_noti(sender, instance, created, **kwargs):
                 descriptions = Descriptions.objects.get(id=cve.id)
         except Descriptions.DoesNotExist:
                 descriptions = None
+        subscribed_user = Follow_Affected.objects.filter(affected__con=cve).values_list('user_id',flat=True).distinct
         message = reformat_tele_message(cve.cve_id, cvss31, descriptions , cve.id)
-        send_message_telegram(message)
+        noti_user = NotiUser.objects.filter(user_id__in=subscribed_user)
+        chat_id= noti_user.chat_id
+        token = noti_user.token_bot
+        send_message_telegram(message, token, chat_id)
     
