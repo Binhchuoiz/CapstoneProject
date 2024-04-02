775a121864888b32b0f952fa505f5c7f1de7ed33
BADly1122 <ducba3011@gmail.com>
2024-04-02 23:39:35 +0700

    update msg in signals.py

--- CVEAlert/CVEAlert/signals.py
+++ CVEAlert/CVEAlert/signals.py
@@ -9,25 +9,23 @@ cve_Updated = Signal()
 
 # @receiver(cve_Updated)
 # def new_cve_noti(sender, **kwargs):
-@receiver(post_save, sender=CVE)
+@receiver(post_save, sender=Metric)
 def new_cve_noti(sender, instance, created, **kwargs):   
-        cve=instance
+        metric=instance
+        cve = CVE.objects.get(id=metric.con_id)
         try:
-                metric = Metric.objects.filter(con_id = cve.id)
-        except:
-                metric = None
-        cvss31 = [m.cvssv31 for m in metric]
-        try:
-                descriptions = Descriptions.objects.get(con_id=cve.id)
+                descriptions = Descriptions.objects.get(con_id=metric.con_id)
         except Descriptions.DoesNotExist:
-                descriptions = None
+                descriptions = None    
+        try:
+                cvssv31 = CvssV31.objects.get(id=metric.cvssv31_id)
+        except:
+                cvssv31 = None
         # subscribed_user = Follow_Affected.objects.filter(affected__con=cve).values_list('user_id',flat=True).distinct()
-        message = reformat_tele_message(cve.cve_id, cvss31, descriptions , cve.id)
+        message = reformat_tele_message(cve.cve_id, cvssv31.base_score, descriptions , cve.id)
         # noti_user = NotiUser.objects.filter(user_id__in=subscribed_user)
         # chat_id= noti_user.chat_id
         # token = noti_user.token_bot
-        print(descriptions)
-        print(cvss31)
         print(message)
         # send_message_telegram(message)
         # send_email(message, "zdemon2002@gmail.com")
