6b9d2558952d59b9d8975933fdd6af4f9efc96a0
hien <zdemon2002@gmail.com>
2024-04-04 22:03:48 +0700

    test signals.py

--- CVEAlert/CVEAlert/signals.py
+++ CVEAlert/CVEAlert/signals.py
@@ -2,7 +2,7 @@ from django.db.models.signals import post_save
 from django.dispatch import receiver,Signal
 
 from .alert_tele import send_message_telegram , reformat_tele_message
-from firstapp.models import CVE , CvssV31 , Descriptions , User, Metric ,Follow_Affected
+from firstapp.models import CVE , CvssV31 , Descriptions , User, Metric ,Follow_Affected,Affected
 from accounts.models import NotiUser
 from .alert_email import send_email
 cve_Updated = Signal()
@@ -21,18 +21,20 @@ def new_cve_noti(sender, instance, created, **kwargs):
                 cvssv31 = CvssV31.objects.get(id=metric.cvssv31_id)
         except:
                 cvssv31 = None
-        # subscribed_user = Follow_Affected.objects.filter(affected__con=cve).values_list('user_id',flat=True).distinct()
-        message = reformat_tele_message(cve.cve_id, cvssv31.base_score, descriptions , cve.id)
-        # for user_id in subscribed_user:
-        list_noti = NotiUser.objects.all()
-        for noti_user in list_noti:
+        subscribed_users = []
+        affected_entities = Affected.objects.filter(con=cve)
+        for affected_entity in affected_entities:
+            subscribed_users += Follow_Affected.objects.filter(affected=affected_entity).values_list('user_id', flat=True).distinct()
+        message = reformat_tele_message(cve.cve_id, cvssv31.base_score, descriptions, cve.id)
+        for user_id in subscribed_users:
+                noti_user = NotiUser.objects.get(pk=user_id)
                 if noti_user.status== "telegram" or noti_user.status == "all":
-                        if   noti_user.token_bot and noti_user.chat_id:
-                                chat_id = noti_user.chat_id
-                                token = noti_user.token_bot
-                        send_message_telegram(message,token,chat_id)
-                        if noti_user.status == "gmail" or noti_user.status=="all":
-                                if noti_user.email_address:
-                                        send_email(message, noti_user.email_address)
+                                if   noti_user.token_bot and noti_user.chat_id:
+                                        chat_id = noti_user.chat_id
+                                        token = noti_user.token_bot
+                                send_message_telegram(message,token,chat_id)
+                                if noti_user.status == "gmail" or noti_user.status=="all":
+                                        if noti_user.email_address:
+                                                send_email(message, noti_user.email_address)
         
     
