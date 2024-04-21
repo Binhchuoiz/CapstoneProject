f48649d92588b398ec4f79c5cb80fd4575765fe0
Nguyen Duc Binh <binhndhe161032@fpt.edu.vn>
2024-04-21 21:59:22 +0700

    FE: fix navbar

--- CVEAlert/CVEAlert/urls.py
+++ CVEAlert/CVEAlert/urls.py
@@ -6,7 +6,7 @@ from django.conf import settings
 
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('admincvecvecvepass/', admin.site.urls),
     path('', include('firstapp.urls')),
     path('account/', include('accounts.urls')),
     path('chat/', include('chat.urls')),
