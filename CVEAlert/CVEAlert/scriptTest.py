6386d4dcd2b01c37cb61703012152825fac976ab
Binhchuoiz <binhndhe161032@fpt.edu.vn>
2024-05-11 21:09:49 +0700

    fix detail cve

--- CVEAlert/CVEAlert/scriptTest.py
+++ CVEAlert/CVEAlert/scriptTest.py
@@ -213,7 +213,7 @@ def read_json_files(folder_path, folder_name):
 # Specify the path to the directory containing JSON files
 # cves_folder_path = r"E:\IAP104\cvelistV5-main\cves"
 # D:\Đồ án\cves\cves
-cves_folder_path = r"D:\Đồ án\cves\cves"
+cves_folder_path = r"E:\FPT_University\Major_SS9\IAP491\Web\db\cvelistV5\cves"
 cves_folder_path = os.path.normpath(cves_folder_path)
 
 try:
